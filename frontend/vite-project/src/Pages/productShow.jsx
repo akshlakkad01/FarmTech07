1229d72054f5e3e98ee519f9897812a032e00bca
krish kadchhi <kadchhikrish@gmail.com>
2024-08-03 22:52:27 +0530

    commited

--- frontend/vite-project/src/Pages/productShow.jsx
+++ frontend/vite-project/src/Pages/productShow.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
+import {Cookies, useCookies} from 'react-cookie';
 import axios from "axios";
 import './productShow.css';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function ProductShow() {
     
-    const [isSignup , setisSingup] = useState(false);
     const [myData , setData] = useState([]);
+    const navigate = useNavigate();
     
-    
+
     const addInCart = async(item)=>
     {
         alert("product added in the cart")
@@ -34,39 +35,41 @@ export default function ProductShow() {
 
     useEffect(()=>
     {
+      if (!document.cookie) {
+          navigate("/singup");
+      }
        axios
        .get("http://localhost:8080/showPro")
        .then((res)=>setData(res.data))
     },[])
 
-    
+    const cookie = document.cookie;
+    console.log(cookie);
         return(
             <>
-            <Navigate to="/singup" />
-          
-            <h1>All Products</h1>
-            {
-                myData.map((item)=>
+              <h1>All Products</h1>
                 {
-                    const {id,name,category,price,image} = item;
-                    
-                    return (
-                        <>
-                        <div className="productList" >
-                         <p key={id}>
-                          id :- {id} <br></br>
-                          Name :-  {name} <br></br>
-                          Category :-   {category} <br></br>
-                          Price :-  {price}/kg <br></br>
-                          {/* <button onClick={()=>deleteData(id)} >Delete</button> */}
-                          <button onClick={()=>addInCart(item)}>Buy</button>
-                        </p>
+                    myData.map((item)=>
+                    {
+                        const {id,name,category,price,image} = item;
                         
-                         </div>
-                        </>
-                    )
-                })
-            }
-            </>
-        )
+                        return (
+                            <>
+                            <div className="productList" >
+                             <p key={id}>
+                              id :- {id} <br></br>
+                              Name :-  {name} <br></br>
+                              Category :-   {category} <br></br>
+                              Price :-  {price}/kg <br></br>
+                              {/* <button onClick={()=>deleteData(id)} >Delete</button> */}
+                              <button onClick={()=>addInCart(item)}>Buy</button>
+                            </p>
+                            
+                             </div>
+                            </>
+                        )
+                    })
+                }
+                </>
+            )
 }
